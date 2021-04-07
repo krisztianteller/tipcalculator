5884dd34ac35690ffef047d753232ab0a6963253
krisztianteller <75849856+krisztianteller@users.noreply.github.com>
2021-04-07 11:31:18 +0200

    rename js file

---
+++ tip.js
@@ -0,0 +1,12 @@
+function calculatetip(){
+    var bilAmt=
+    document.getElementById("billamount").value;
+    var service=
+    document.getElementById("serviceQuali").value;
+    var numberOfPeople=
+    document.getElementById("peopleamt").value;
+}
+
+if(bilAmt == " " || service == 0) {
+    alert("Please enter values")
+}
